80b1538e2043e35b98cd03d2ce8af346f0690b1d
Oumar Barry <oumar.barry1202@gmail.com>
2023-11-25 01:59:14 +0000

    chore: update dependencies

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,41 +1,41 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress"
 
 export default defineConfig({
-  title: 'Polar',
-  description: 'Polar is a platform for open source maintainers to get better funding.',
+  title: "Polar",
+  description: "Polar is a platform for open source maintainers to get better funding.",
 
   lastUpdated: true,
   cleanUrls: true,
 
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+    ["link", { rel: "icon", type: "image/png", href: "/favicon.png" }],
   ],
 
   themeConfig: {
     siteTitle: false,
-    logo: { light: '/polar-logo-blue.svg', dark: '/polar-logo-white.svg' },
+    logo: { light: "/polar-logo-blue.svg", dark: "/polar-logo-white.svg" },
 
     sidebar: [
       {
-        text: 'Polar',
+        text: "Polar",
         items: [
-          { text: 'Documentation', link: '/' },
-          { text: 'API', link: '/api' },
-          { text: 'GitHub Action', link: '/github-action' },
+          { text: "Documentation", link: "/" },
+          { text: "API", link: "/api" },
+          { text: "GitHub Action", link: "/github-action" },
         ],
       },
     ],
 
     outline: {
-      level: 'deep',
-      label: 'Table of contents',
+      level: "deep",
+      label: "Table of contents",
     },
 
-    search: { provider: 'local' },
+    search: { provider: "local" },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/polarsource/polar' },
-      { icon: 'x', link: 'https://twitter.com/@polar_sh' },
+      { icon: "github", link: "https://github.com/polarsource/polar" },
+      { icon: "x", link: "https://twitter.com/@polar_sh" },
     ],
   },
 })
